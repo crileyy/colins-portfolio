7ee9cb099cc51cf42e3ff59b1892b87ee41a3db6
crileyy <criley0007@gmail.com>
2025-06-03 16:18:29 -0400

    updated about and ai-chatbot text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,14 +4,75 @@ export default function About() {
       <div>
         <h1>About Me</h1>
         <p>
-          intro blurb about me
+          {`I started my computer science journey in high school when my math class had a project involving creating fractal
+          patterns using the graphical programming language Scratch. I enjoyed using Scratch so much, that I ended up creating
+          a round based zombies top-down shooter game, emulating the Call of Duty Zombies video game. From that moment, I fell
+          in love with the logical processes and problem solving of programming. I continued taking more computer science classes
+          in high school, over the summer at Stanford, and eventually at Northeastern University, where I majored in computer science.`}
         </p>
         <p>
-          {'why I\'m passionate about frontend'}
+          {`I started my professional software developer experience as a backend developer building REST APIs and microservices during
+          my first co-op at Intuit, but I was always interested in the frontend. During my second co-op at Intuit, I took the opportunity
+          to enroll in a 2 week class that tought JavaScript and React and from there I knew that's what I wanted to focus on. I love the
+          aspect of frontend development that changes are instantly visable to see and that users will be interacting directly with those
+          changes. I particularly enjoy the component architecture of React and getting to focus on what building blocks I want to use to
+          build the UI, as opposed to thinking about how exactly the UI is rendered (although I still think about that too). To me, it makes
+          building a UI like building with Lego blocks. I honed my React skills while working at Meta in 2019, where I built a web app for
+          Meta data center maintenance requests. I've been working as a full time frontend engineer since then. I continue to stay up to date
+          on my frontend skills through taking online courses in my free time, such as Kent C Dodd's Epic React and Josh W Comeau's CSS for
+          JavaScript Developers.`}
         </p>
         <h2>Work Experience</h2>
+        <p>
+          {`The Northeastern University curriculum heavily involves work experience in the form of co-ops. Co-ops are typically a 6 month long
+          professional work experience that are built into the undergraduate experience. While part of the curriculum, I still had to apply to
+          job postings, complete coding challenges, and go through technical and behavioral interviews before being hired for these roles that
+          were paid and required working 40 hours a week. I like to think of co-op as in between an internship and full time experience. While
+          on a co-op, I'm there long enough to take on more responsibilities than an intern, but I'm not full time. As a result of the co-op
+          program, my work experience as a software engineer started during my second year of college when I got my first co-op at Intuit in
+          San Diego. Read more below to learn about my full time, co-op, and internship experiences.`}
+        </p>
+        <h3>Full Time</h3>
+        <h4>Disney+</h4>
+        <h4>ASICS Digital</h4>
+        <h3>Co-ops and Internships</h3>
+        <h4>Spare-it</h4>
+        <h4>Meta</h4>
+        <h4>ASICS Digital</h4>
+        <h4>Intuit</h4>
+        <h4>Intuit</h4>
         <h2>Other Interests</h2>
-        <p>music, guitar, and youtube channel</p>
+        <p>
+          {`Besides software development, I've always had a passion for music, YouTube, and video games. In middle school and high school, I
+          combined my passion for video games and YouTube by creating my own gaming channel, where I would upload Call of Duty game play videos.
+          This process tought me how to record content, edit videos, add voiceovers, and create video thumbnails using photoshop. It was always for
+          fun and a hobby of mine, but I managed to amass 32,000 subscribers during my time uploading gaming videos.`}
+        </p>
+        <p>
+          {`I stopped uploading gaming videos once I started college, but that's when I was able to pursue another passion of mine, music.
+          I'd always played music throughout my life, starting with the violin in elementary school, then the drums in a rock band with my
+          friends in middle school, and then the trumpet in jazz band in high school. Once college started, there wasn't a structured time
+          for me to play music like there was previously, but I had always wanted to play guitar and it just so happened that my suitemate
+          in my freshman year dorm room knew how to play. Through my college suitemate acting as a motivator and YouTube tutorials, I tought
+          myself how to play electric guitar. I found that the best way for me to learn was to learn how to play the guitar parts for songs
+          I like. I particularly like listening and playing rock and indie rock music, such as, The Strokes, Arctic Monkeys, Last Dinosaurs, etc.
+          Combine that with being able to play multiple guitar parts along with my roommate, and I had the motivation I needed to learn.
+          After a couple years of playing guitar and being able to learn and play most of the songs I like to listen to, I decided to start
+          uploading guitar covers to a new YouTube channel, bringing back my old passion of YouTube and making videos. It was also a way for
+          me to keep track of the songs I knew how to play. It taught me how to record to an audio interface, edit videos with music, and
+          music production. Learning guitar, recording covers, and producing guitar audio even inspired me to get a music minor during my time
+          at Northeastern. You can checkout my guitar cover channel below:`}
+        </p>
+        <iframe
+          allowFullScreen
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/noJesJ5WtRc?si=XACDIEFLT3K3D-9N"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          style={{ border: 0 }}
+        />
       </div>
     </>
   );
